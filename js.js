8617a3383a59b5b21a36a014f8d9f32ff786f669
yaqip777 <yaqip777@gmail.com>
2026-04-21 19:48:44 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,11 +1,14 @@
-let coin = localStorage.getItem("coin") || 0;
-let power = localStorage.getItem("power") || 1;
+let coin = Number(localStorage.getItem("coin")) || 0;
+let power = Number(localStorage.getItem("power")) || 1;
 
-coin = Number(coin);
-power = Number(power);
+// Sahifa yuklanganda sonni ko'rsatish
+window.onload = function() {
+    document.getElementById("coin").innerText = coin;
+};
 
 function updateCoin() {
     document.getElementById("coin").innerText = coin;
+    // XOTIRAGA SAQLASH
     localStorage.setItem("coin", coin);
     localStorage.setItem("power", power);
 }
@@ -13,8 +16,6 @@ function updateCoin() {
 function tap() {
     coin += power;
     updateCoin();
-
-    showPlus(power); // 🔥 faqat power chiqadi
 }
 
 function upgrade() {
@@ -22,24 +23,7 @@ function upgrade() {
         coin -= 10;
         power++;
         updateCoin();
+    } else {
+        alert("Coin yetarli emas!");
     }
 }
-
-function showPlus(value) {
-    let plus = document.createElement("div");
-    plus.innerText = "+" + value;
-
-    plus.style.position = "absolute";
-    plus.style.color = "yellow";
-    plus.style.fontSize = "20px";
-    plus.style.left = "50%";
-    plus.style.top = "60%";
-
-    document.body.appendChild(plus);
-
-    setTimeout(() => {
-        plus.remove();
-    }, 800);
-}
-
-updateCoin();
