bc86130c5d11ad9650ac5482308ff9f16e8e1c44
yaqip777 <yaqip777@gmail.com>
2026-04-21 13:20:46 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,13 +1,17 @@
+// 1. Eng tepada: Avval xotiradan ma'lumotni qidiramiz
 let coin = Number(localStorage.getItem("coin")) || 0;
 let power = Number(localStorage.getItem("power")) || 1;
 
-// Sahifa yuklanganda darhol ekranga chiqarish
-document.getElementById("coin").innerText = coin;
+// 2. Sahifa ochilishi bilan ekranda eski sonni ko'rsatamiz
+window.onload = function() {
+    document.getElementById("coin").innerText = coin;
+};
 
 function updateCoin() {
     // Ekranni yangilash
     document.getElementById("coin").innerText = coin;
-    // FAQAT shu yerda xotiraga saqlash kerak
+    
+    // 3. XOTIRAGA SAQLASH (Eng muhim joyi)
     localStorage.setItem("coin", coin);
     localStorage.setItem("power", power);
 }
