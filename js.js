31874dea026bfeb4d886490074bea59742859eab
yaqip777 <yaqip777@gmail.com>
2026-04-21 19:33:07 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,29 +1,55 @@
-let coin = Number(localStorage.getItem("coin")) || 0;
-let power = Number(localStorage.getItem("power")) || 1;
+let coin = localStorage.getItem("coin") || 0;
+coin = Number(coin);
 
-// Sahifa yuklanganda sonni ko'rsatish
-window.onload = function() {
-    document.getElementById("coin").innerText = coin;
-};
+let power = localStorage.getItem("power") || 1;
+power = Number(power);
 
+// coin ko‘rsatish
 function updateCoin() {
     document.getElementById("coin").innerText = coin;
-    // XOTIRAGA SAQLASH
     localStorage.setItem("coin", coin);
-    localStorage.setItem("power", power);
 }
 
+// bosganda coin qo‘shadi
 function tap() {
     coin += power;
     updateCoin();
+
+    // +effect
+    let plus = document.createElement("div");
+    plus.innerText = "+" + power;
+    plus.style.position = "absolute";
+    plus.style.color = "yellow";
+    plus.style.left = "50%";
+    plus.style.top = "50%";
+
+    document.body.appendChild(plus);
+
+    setTimeout(() => plus.remove(), 500);
 }
 
+// upgrade
 function upgrade() {
-    if (coin >= 10) {
-        coin -= 10;
+    let cost = power * 10;
+
+    if (coin >= cost) {
+        coin -= cost;
         power++;
+        localStorage.setItem("power", power);
         updateCoin();
     } else {
         alert("Coin yetarli emas!");
     }
 }
+
+// offline earning
+let last = localStorage.getItem("last") || Date.now();
+let now = Date.now();
+
+let diff = Math.floor((now - last) / 1000);
+coin += diff * power;
+
+localStorage.setItem("last", now);
+
+// start
+updateCoin();
