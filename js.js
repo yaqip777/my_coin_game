f5de4161f8b851b0f9dedd336a8fa002df66c115
yaqip777 <yaqip777@gmail.com>
2026-04-21 19:12:46 +0300

    Update js.js

--- js.js
+++ js.js
@@ -13,32 +13,9 @@ function updateCoin() {
     localStorage.setItem("power", power);
 }
 
-function tap(event) {
+function tap() {
     coin += power;
     updateCoin();
-
-    // EFFEKT YARATISH
-    createClickEffect(event);
-}
-
-function createClickEffect(event) {
-    const effect = document.createElement("div");
-    effect.classList.add("click-animation");
-    effect.innerText = "+" + power;
-
-    // Bosilgan joyni aniqlash
-    let x = event.clientX  (event.touches && event.touches[0].clientX)  window.innerWidth / 2;
-    let y = event.clientY  (event.touches && event.touches[0].clientY)  window.innerHeight / 2;
-
-    effect.style.left = x + "px";
-    effect.style.top = y + "px";
-    effect.style.position = "absolute"; // CSS da bo'lmasa, shu yerda bo'lgani ma'qul
-
-    document.body.appendChild(effect);
-
-    setTimeout(() => {
-        effect.remove();
-    }, 800);
 }
 
 function upgrade() {
