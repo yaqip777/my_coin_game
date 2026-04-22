d3b064e6d286e72f1ff5a50935b4c714db7a38b3
yaqip777 <yaqip777@gmail.com>
2026-04-22 15:16:11 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,29 +1,46 @@
-let coin = Number(localStorage.getItem("coin")) || 0;
-let power = Number(localStorage.getItem("power")) || 1;
+// O'yin yuklanganda saqlangan ballni olish, agar bo'lmasa 0 deb olish
+let score = localStorage.getItem('myScore') ? parseInt(localStorage.getItem('myScore')) : 0;
 
-// Sahifa yuklanganda sonni ko'rsatish
-window.onload = function() {
-    document.getElementById("coin").innerText = coin;
-};
+// Sahifa yuklanganda ballni ekranda ko'rsatish
+document.getElementById('coin').innerText = score;
 
-function updateCoin() {
-    document.getElementById("coin").innerText = coin;
-    // XOTIRAGA SAQLASH
-    localStorage.setItem("coin", coin);
-    localStorage.setItem("power", power);
+function tap(event) {
+    score++;
+    
+    // Yangi ballni ekranga chiqarish
+    document.getElementById('coin').innerText = score;
+    
+    // Ballni xotiraga (localStorage) saqlash
+    localStorage.setItem('myScore', score);
+
+    // Effekt chiqarish funksiyasi
+    createScoreEffect(event);
 }
 
-function tap() {
-    coin += power;
-    updateCoin();
+function createScoreEffect(event) {
+    const effect = document.createElement('div');
+    effect.innerText = '+1';
+    effect.className = 'score-animation';
+
+    let x, y;
+    if (event.touches && event.touches.length > 0) {
+        x = event.touches[0].clientX;
+        y = event.touches[0].clientY;
+    } else {
+        x = event.clientX;
+        y = event.clientY;
+    }
+
+    effect.style.left = x + 'px';
+    effect.style.top = y + 'px';
+
+    document.body.appendChild(effect);
+
+    setTimeout(() => {
+        effect.remove();
+    }, 800);
 }
 
 function upgrade() {
-    if (coin >= 10) {
-        coin -= 10;
-        power++;
-        updateCoin();
-    } else {
-        alert("Coin yetarli emas!");
-    }
+    alert("Tez orada yangi funksiyalar qo'shiladi!");
 }
